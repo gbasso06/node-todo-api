df31905c8053a9c9f6b326c5a2e5ad1aa6eda5d2
Gustavo Basso <gbasso06@gmail.com>
2017-08-08 09:42:46 -0300

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
@@ -79,6 +80,24 @@ UserSchema.statics.findByToken = function (token) {
 	});
 
 };
+
+UserSchema.pre('save', function (next){
+	var user = this;
+
+	// checks if password is modified
+	if(user.isModified('password')){
+
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
+
 
 var User = mongoose.model('User', UserSchema);
 
