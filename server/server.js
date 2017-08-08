427b60896491d87ca49d362398212fc44644af47
Gustavo Basso <gbasso06@gmail.com>
2017-08-08 10:36:51 -0300

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-require('./config/config');
-
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
+const config = require('./config/config');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
