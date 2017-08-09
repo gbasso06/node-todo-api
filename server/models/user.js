0f2a125099cc065018273afd17d0ac8e507fd555
Gustavo Basso <gbasso06@gmail.com>
2017-08-09 09:27:33 -0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -78,8 +78,29 @@ UserSchema.statics.findByToken = function (token) {
 		'tokens.token': token,
 		'tokens.access': 'auth'
 	});
-
 };
+
+UserSchema.statics.findByCredentials = function (email,password) {
+	var User = this;
+
+	return User.findOne({email}).then((user) => {
+		if(!user){
+			return Promise.reject();
+		}
+
+		return new Promise((resolve, reject) => {
+			// Compare password and user.password
+			bcrypt.compare(password, user.password, (err, res) => {
+
+				if(res) resolve(user);
+				else{
+					return reject()
+				}
+			});
+		});
+	})
+};
+
 
 UserSchema.pre('save', function (next){
 	var user = this;
